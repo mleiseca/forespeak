aab95f7e8994e46e534295db6cb706fd6f4ff705
Michael Leiseca <mleiseca@mleiseca.gateway.2wire.net>
2011-03-14 19:55:45 -0500

    first steps on testing outcome all user positions

--- spec/models/outcome_spec.rb
+++ spec/models/outcome_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Outcome do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @market   = Factory.build(:market)
+    @outcome1 = Factory.build(:outcome, :description =>"Outcome1")
+    @outcome2 = Factory.build(:outcome, :description =>"Outcome2")
+    
+    @market.outcomes = [@outcome1, @outcome2]
+    
+    @market.save!
+    
+  end
+
+  describe "all_user_positions" do
+    it "should return an empty array if there have been no transactions" do
+      @outcome1.all_user_positions.size.should == 0
+    end
+
+  #   it "should return the most recent positions for each user" do
+  #     
+  # 
+  #     
+  #   end
+  end
+  
 end
